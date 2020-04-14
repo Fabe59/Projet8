60af20a55c369a3dc4dae2f1e0150a874928f4eb
Fabe59 <pringuetfabrice@gmail.com>
2020-04-15 01:36:48 +0200

    refactoring

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'OCP8',
         'USER': 'fabricepringuet',
-        'PASSWORD': 'Marsbynight13',
+        'PASSWORD': '%',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
