8b0b72db99a235b79ebe694110b6dbe4245e84e1
Fabe59 <pringuetfabrice@gmail.com>
2020-05-18 18:54:52 +0200

    settings modified

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, 'staticfiles'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_REDIRECT_URL = 'food:home'
 LOGIN_URL = 'users:login'
