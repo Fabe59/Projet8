42bd0415b6faca4a0e7da2c469802dd6f56f47da
Fabe59 <pringuetfabrice@gmail.com>
2020-06-04 12:12:39 +0200

    delete sentry test

--- pur_beurre/food/urls.py
+++ pur_beurre/food/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
 
 app_name = 'food'
 urlpatterns = [
@@ -11,5 +9,4 @@ urlpatterns = [
     path('product/<int:id>', views.show, name="show"),
     path('save/', views.save, name="save"),
     path('legals/', views.legals, name="legals"),
-    path('sentry-debug/', trigger_error),
 ]
