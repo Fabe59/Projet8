5e4a626d99e7526ede33ac738ee056cec8a46a9c
Fabe59 <pringuetfabrice@gmail.com>
2020-06-09 23:42:39 +0200

    settings updated

--- pur_beurre/pur_beurre/settings/__init__.py
+++ pur_beurre/pur_beurre/settings/__init__.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'purbeurre',
-        'USER': 'fp',
-        'PASSWORD': 'megamotdepasse',
+        'USER': '*',
+        'PASSWORD': '*',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
